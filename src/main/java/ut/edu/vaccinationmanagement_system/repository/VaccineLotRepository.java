e9e697cbb271d959a280456bd347332487445d2c
nguuyenn <nguyennt6041@ut.edu.vn>
2026-01-03 22:04:46 +0700

    Cải thiện

--- src/main/java/ut/edu/vaccinationmanagement_system/repository/VaccineLotRepository.java
+++ src/main/java/ut/edu/vaccinationmanagement_system/repository/VaccineLotRepository.java
@@ -1,27 +1,18 @@
 package ut.edu.vaccinationmanagement_system.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ut.edu.vaccinationmanagement_system.entity.VaccineLot;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface VaccineLotRepository extends JpaRepository<VaccineLot, Long> {
 
-    Optional<VaccineLot> findByLotNumber(String lotNumber);
+    // Cảnh báo sắp hết hạn
+    @Query("SELECT v FROM VaccineLot v WHERE v.expiryDate <= :date")
+    List<VaccineLot> findExpiringSoon(LocalDate date);
 
-    // Lô sắp hết hạn
-    @Query("""
-        SELECT v FROM VaccineLot v
-        WHERE v.expiryDate <= :date
-          AND v.status = 'AVAILABLE'
-    """)
-    List<VaccineLot> findExpiringBefore(@Param("date") LocalDate date);
-
-    // Lô sắp hết số lượng
-    List<VaccineLot> findByRemainingQuantityLessThanEqual(int threshold);
+    // Cảnh báo sắp hết số lượng
+    List<VaccineLot> findByRemainingQuantityLessThanEqual(Integer threshold);
 }
